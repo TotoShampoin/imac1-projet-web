ae99c2bcca3bebd024bad524c7862a0f512acd9b
TotoShampoin <thomasgarnier2010@hotmail.fr>
2023-05-23 16:07:31 +0200

    finished with the sql

--- database/donnees.sql
+++ database/donnees.sql
@@ -28,8 +28,8 @@ INSERT INTO Ingredient (libelle, stock) VALUES
 ('Pain', 10),
 ('Viande rouge', 10),
 ('Viande blanche', 10),
-('Poulet', 10),
 ('Poisson', 10),
+('Steak de soja', 10),
 ('Salade', 10),
 ('Tomate', 10),
 ('Oignon', 10),
@@ -45,3 +45,10 @@ INSERT INTO Ingredient (libelle, stock) VALUES
 ('Cookie', 10),
 ('Muffin', 10),
 ('Donut', 10);
+
+INSERT INTO AlimentIngredient (aliid, ingid) VALUES
+( 1,  1), ( 1,  2), ( 1,  6), ( 1,  7), ( 1,  8), ( 1,  9), ( 1, 10),
+( 2,  1), ( 2,  3), ( 2,  6), ( 2,  7), ( 2,  8), ( 2,  9), ( 2, 10),
+( 3,  1), ( 3,  4), ( 3,  6), ( 3,  7), ( 3,  8), ( 3,  9), ( 3, 10),
+( 4,  1), ( 4,  5), ( 4,  6), ( 4,  7), ( 4,  8), ( 4,  9), ( 4, 10),
+( 5, 13), ( 6, 14), ( 7, 15), ( 8, 16), ( 9, 17), (10, 18), (11, 19), (12, 20);
